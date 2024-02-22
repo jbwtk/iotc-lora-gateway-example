5581ca413bb83c9621743b2c5df841caf311d27a
James Barrett <noparadise+wtk@gmail.com>
2024-02-22 13:28:30 +0000

    revert settings

--- meta-iotconnect-lora-demo/recipes-apps/lora-demo/files/local_settings.py
+++ meta-iotconnect-lora-demo/recipes-apps/lora-demo/files/local_settings.py
@@ -1,30 +1,30 @@
 
+
 '''
 IOTConnect config
 '''
 # SDK config
 cpid = 'avtds'
 env = 'avnetpoc'
-sid = 'Yjg5MmMzNThlMzc1NGNjMzg4NDEzMmUyNzFlMjYxNTE=UDI6MDM6NzAuMTQ='
+sid = 'Yjg5M...NzAuMTQ='
 
 # API config
 iotc_config = {
     "company_name": "Witekio",
-    "solution_key": "RjQ5QkE1M0EtNThFRC00RDRBLThBMTQtOEZDRTlDQUFDMEEyLWFjY2Vzc0tFWS1jaTJ2djdxNmFj",
+    "solution_key": "RjQ...dxNmFj",
     "company_guid": "b892c358-e375-4cc3-8841-32e271e26151"
 }
 
 '''
 LNS configuration.
 '''
-server_ip = "192.168.1.8"  # This points to the chirpstack interface.
+server_ip = "localhost"  # This points to the chirpstack interface.
 
 # Global API key from chirpstack LNS:
 # API key ID be808ccb-e5b5-40c3-995a-0c5c7b54c2a8
-# API key ID 53960b6d-65f4-4ed3-8d05-77429ee45322
-chirpstack_api_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5X2lkIjoiNTM5NjBiNmQtNjVmNC00ZWQzLThkMDUtNzc0Mj' \
-    'llZTQ1MzIyIiwiYXVkIjoiYXMiLCJpc3MiOiJhcyIsIm5iZiI6MTcwNzkxNjMwNiwic3ViIjoiYXBpX2tleSJ9.jeauvRqLwYNGAip2oaJ01NG0RmP' \
-    'Wh1EC3WfkpDDcLwM'
+chirpstack_api_token = 'eyJhbGc...Tk5N' \
+            'WEtMGM1YzdiNTRjMmE...J9.5' \
+            'RcNJdyLkPU8ciNsiDE...HEhQ'
 
 min_transmit = 30
 
